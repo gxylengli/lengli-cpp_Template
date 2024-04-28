5678cf5719da2be971190c4333de66618197a7ca
gxylengli <96274730+gxylengli@users.noreply.github.com>
2024-04-28 10:11:08 +0800

    Update Data_structure.cpp

--- Data_structure.cpp
+++ Data_structure.cpp
@@ -91,7 +91,7 @@ struct BIT{
 	
 }tr;
 
-//动态二维前缀和
+//动态二维前缀和单点查询
 
 struct presum_dynamic{
     int n;
@@ -123,6 +123,59 @@ struct presum_dynamic{
 		updata(c+1,d+1,k);
 	}
 }tr;
+
+//动态二维前缀和区间查询
+
+template<class T>
+struct presum_dynamic {
+    vector<vector<T>> c1, c2, c3, c4;
+    int szx, szy;
+    presum_dynamic() {}
+    presum_dynamic(int n, int m) {
+        init(n, m);
+    }
+    void init(int n, int m) {
+        szx = n;
+        szy = m;
+        c1.clear(), c2.clear(), c3.clear(), c4.clear();
+        c1.resize(n + 2, vector<T> (m + 2, 0));
+        c2.resize(n + 2, vector<T> (m + 2, 0));
+        c3.resize(n + 2, vector<T> (m + 2, 0));
+        c4.resize(n + 2, vector<T> (m + 2, 0));
+    }
+    void modify(int x, int y, T s) {
+        for (int i = x; i <= szx; i += (i & -i)) {
+            for (int j = y; j <= szy; j += (j & -j)) {
+                c1[i][j] += s;
+                c2[i][j] += s * y;
+                c3[i][j] += s * x;
+                c4[i][j] += s * x * y;
+            }
+        }
+    }
+    T query(int x, int y) {
+        T s = 0;
+        for (int i = x; i; i -= (i & -i)) {
+            for (int j = y; j; j -= (j & -j)) {
+                s += c1[i][j] * (x + 1) * (y + 1);
+                s -= c2[i][j] * (x + 1);
+                s -= c3[i][j] * (y + 1);
+                s += c4[i][j];
+            }
+        }
+        return s;
+    }
+    void recmodify(int a, int b, int c, int d, T s) {
+        modify(a, b, s);
+        modify(a, d + 1, -s);
+        modify(c + 1, b, -s);
+        modify(c + 1, d + 1, s);
+    }
+    T recquery(int a, int b, int c, int d) {
+        return query(c, d) - query(a - 1, d) - query(c, b - 1) + query(a - 1, b - 1);
+    }
+};
+
 
 //珂朵莉树
 struct Node
