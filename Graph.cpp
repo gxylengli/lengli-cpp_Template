8f78ef512979c7b700ae808951e29f7fb4b2de89
lengli <791401131@qq.com>
2024-08-04 21:31:48 +0800

    update

--- Graph.cpp
+++ Graph.cpp
@@ -524,54 +524,55 @@ struct V_DCC{
 // Tarjan求有向图强连通分量SCC
 
 struct Tarjan{
-    vector<int> eg[N];
+    std::vector<int> eg[N];
     int dfn[N],low[N],timespace;
     int stk[N],top;
     bool st[N];
     int color[N];
     int scc_cnt,n;
-    vector<int> scc[N];
+    std::vector<int> scc[N];
     void init(int x){
         timespace=1;scc_cnt=0;
         top=0;
         n=x;
         for(int i=1;i<=n;i++) eg[i].clear(),scc[i].clear();
         for(int i=1;i<=n;i++){
-        	dfn[i]=0,low[i]=0;
-        	color[i]=0;
+            dfn[i]=0,low[i]=0;
+            color[i]=0;
         }
     }
     void add(int a,int b){
         eg[a].push_back(b);
     }
     void dfs(int u){
-		low[u]=dfn[u]=++timespace;
-		stk[++top]=u;
-		st[u]=1;
-		for(auto v:eg[u]){
-			if (!dfn[v]){
-				dfs(v);
-				low[u]=min(low[u],low[v]);
-			}else if(st[v]) low[u]=min(low[u],low[v]);
-		}
-		if(dfn[u]==low[u]){
-			scc_cnt++;
-			int y;
-			while (y=stk[top--])
-			{
-				scc[scc_cnt].push_back(y);
-				color[y]=scc_cnt;
-				st[y]=0;
-				if (u==y) break;
-			}
-		}
-	}
+        low[u]=dfn[u]=++timespace;
+        stk[++top]=u;
+        st[u]=1;
+        for(auto v:eg[u]){
+            if (!dfn[v]){
+                dfs(v);
+                low[u]=std::min(low[u],low[v]);
+            }else if(st[v]) low[u]=std::min(low[u],low[v]);
+        }
+        if(dfn[u]==low[u]){
+            scc_cnt++;
+            int y;
+            while (y=stk[top--])
+            {
+                scc[scc_cnt].push_back(y);
+                color[y]=scc_cnt;
+                st[y]=0;
+                if (u==y) break;
+            }
+        }
+    }
     void tarjan(){
         for(int i=1;i<=n;i++){
             if(!dfn[i]) dfs(i);
         }
     }
 }tj;
+
 
 
 //线段树优化建图区间连边
