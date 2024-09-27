d855e17dc7c57a7cd0d3d8294b4e093c959c0fd3
lengli <791401131@qq.com>
2024-09-27 20:18:38 +0800

    update

--- Graph.cpp
+++ Graph.cpp
@@ -284,6 +284,42 @@ struct HLD{
     }
 }hld;
 
+//二分图最大匹配HK
+
+struct HopcroftKarp {//idx_start_0
+	std::vector<int> g,l,r;
+	int ans;
+	HopcroftKarp(int n,int m,const std::vector<std::array<int,2>> &e): g(e.size()),l(n, -1),r(m, -1),ans(0){
+		std::vector<int> deg(n+1);
+		for(auto &[x, y]:e) deg[x]++;
+		for(int i=1;i<=n;i++) deg[i]+=deg[i-1];
+		for(auto &[x, y]:e) g[--deg[x]]=y;
+		std::vector<int> q(n);
+		while(true){
+			std::vector<int> a(n,-1),p(n,-1);
+			int t=0;
+			for(int i=0;i<n;i++) if(l[i]==-1)
+				q[t++]=a[i]=p[i] = i;
+			bool match=false;
+			for(int i=0;i<t;i++) {
+				int x = q[i];
+				if(~l[a[x]]) continue;
+				for(int j=deg[x];j<deg[x+1];j++) {
+					int y=g[j];
+					if(r[y]==-1) {
+						while(~y) r[y]=x,std::swap(l[x],y),x=p[x];
+						match=true,ans++;
+						break;
+					}
+					if(p[r[y]]==-1)
+						q[t++]=y=r[y],p[y]=x,a[y]=a[x];
+				}	
+			}
+			if(!match) break;
+		}
+	}
+};
+
 //最大流:节点编号(1-n)
 
 struct MF {
