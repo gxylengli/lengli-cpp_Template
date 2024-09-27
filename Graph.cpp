ec14d83ab060a27539cd0e0bd776b10c65b1673b
lengli <791401131@qq.com>
2024-09-28 00:57:53 +0800

    update

--- Graph.cpp
+++ Graph.cpp
@@ -286,23 +286,23 @@ struct HLD{
 
 //二分图最大匹配HK
 
-struct HopcroftKarp {//idx_start_0
+struct HopcroftKarp {//idx_start_1
 	std::vector<int> g,l,r;
 	int ans;
-	HopcroftKarp(int n,int m,const std::vector<std::array<int,2>> &e): g(e.size()),l(n, -1),r(m, -1),ans(0){
-		std::vector<int> deg(n+1);
+	HopcroftKarp(int n,int m,const std::vector<std::array<int,2>> &e): g(e.size()+2),l(n+2, -1),r(m+2, -1),ans(0){
+		std::vector<int> deg(n+2);
 		for(auto &[x, y]:e) deg[x]++;
-		for(int i=1;i<=n;i++) deg[i]+=deg[i-1];
+		for(int i=1;i<=n+1;i++) deg[i]+=deg[i-1];
 		for(auto &[x, y]:e) g[--deg[x]]=y;
-		std::vector<int> q(n);
+		std::vector<int> q(n+2);
 		while(true){
-			std::vector<int> a(n,-1),p(n,-1);
+			std::vector<int> a(n+2,-1),p(n+2,-1);
 			int t=0;
-			for(int i=0;i<n;i++) if(l[i]==-1)
-				q[t++]=a[i]=p[i] = i;
+			for(int i=1;i<=n;i++) if(l[i]==-1)
+				q[t++]=a[i]=p[i]=i;
 			bool match=false;
 			for(int i=0;i<t;i++) {
-				int x = q[i];
+				int x=q[i];
 				if(~l[a[x]]) continue;
 				for(int j=deg[x];j<deg[x+1];j++) {
 					int y=g[j];
